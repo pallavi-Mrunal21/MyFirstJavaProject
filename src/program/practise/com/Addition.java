973eaf6de84df7b7b2b735f20ffd0456ccd2989d
pallavi-Mrunal21 <tattapurepallavi@gmail.com>
2023-04-07 11:39:38 -0700

    first commit

---
+++ src/program/practise/com/Addition.java
@@ -0,0 +1,12 @@
+package program.practise.com;
+
+public class Addition {
+public static void main (String [] args)
+{
+	int a=10;
+	int b=20;
+	int number=0;
+	number = a+b;
+	System.out.println("Addition is:"+number);
+}
+}
